52af574db6768e8dcb65558a38ba37e485eb7d02
quanming <qmtan17@gmail.com>
2023-09-02 17:04:21 +0800

    font, styles & stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: Text('Hello World'),
+void main() => runApp(MaterialApp(
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test app'),
+        centerTitle: true,
+        backgroundColor: Colors.teal[600],
+      ),
+      body: Center(
+          child: Text(
+        "Helosso",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 2,
+          color: Colors.grey[600],
+          fontFamily: 'OpenSans',
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal[600],
+        onPressed: () {},
+        child: Text("Click"),
+      ),
+    );
+  }
+}
