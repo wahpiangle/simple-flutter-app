3f27afb1e27acc19343dc199113519b1768962c4
quanming <qmtan17@gmail.com>
2023-09-03 14:22:36 +0800

    simpleApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,102 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: Card(),
     ));
 
-class Home extends StatelessWidget {
+class Card extends StatelessWidget {
+  const Card({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('test apps'),
+        title: Text('My app'),
         centerTitle: true,
-        backgroundColor: Colors.teal[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Image.asset('assets/img.jpg'),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.cyan,
-              child: Text('1'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/img.jpg'),
+                radius: 50,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.amber,
-              child: Text('1'),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.green,
-              child: Text('1'),
+            Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
             ),
-          ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.teal[600],
-        onPressed: () {},
-        child: Text("Click"),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Mario',
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Current Level',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '9',
+              style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.email_outlined,
+                  color: Colors.grey[400],
+                  size: 30,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'person@email.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
